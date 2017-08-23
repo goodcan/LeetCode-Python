06ac4ecd7fb6b98b2d9abc730d9e82e21ae58b3d
cancan <1048042379@qq.com>
2017-08-23 19:51:07 +0800

    finish problem 1 and 7

---
+++ 3.Longest Substring Without Repeating Characters/method_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
