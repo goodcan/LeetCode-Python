16103bf8944e93b7c97802207af0e7cbb85a8675
cancan <1048042379@qq.com>
2017-08-24 17:23:50 +0800

    finish problem 3

--- 3.Longest Substring Without Repeating Characters/method_1.py
+++ 3.Longest Substring Without Repeating Characters/method_1.py
@@ -7,3 +7,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        l = range(len(s))
+        left = 0
+        v_i = {}
+        ans = 0
+        for index, value in enumerate(s):
+
+            if value in v_i and v_i[value] >= left:
+                left = v_i[value] + 1
+            v_i[value] = index
+            ans = max(ans, index - left + 1)
+
+        return ans
