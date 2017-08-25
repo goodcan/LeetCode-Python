9035a052d958d767f1d082c7faabbd0cf70784a1
cancan <1048042379@qq.com>
2017-08-25 22:29:52 +0800

    add question annotation

--- 7.Reverse Integer/method_1.py
+++ 7.Reverse Integer/method_1.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+Note:
+The input is assumed to be a 32-bit signed integer.
+Your function should return 0 when the reversed integer overflows.
+"""
+
 class Solution(object):
     def reverse(self, x):
         """
