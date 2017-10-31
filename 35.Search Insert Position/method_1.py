d1883f35d21a7c1af02c8dfea3c4454cdb0c44dc
cancan_home <1048042379@qq.com>
2017-10-31 22:16:50 +0800

    finish problem 35

---
+++ 35.Search Insert Position/method_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Question:
+给定一个排序数组和一个目标值，如果找到目标，返回索引。如果没有，则返回如果插入顺序的索引。
+
+Example:
+    [1,3,5,6], 5 → 2
+    [1,3,5,6], 2 → 1
+    [1,3,5,6], 7 → 4
+    [1,3,5,6], 0 → 0
+"""
+
+from bisect import bisect_left
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        # 第一个参数必须是排序后的列表
+        return bisect_left(nums, target)
---
+++ 35.Search Insert Position/method_2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Question:
+给定一个排序数组和一个目标值，如果找到目标，返回索引。如果没有，则返回如果插入顺序的索引。
+
+Example:
+    [1,3,5,6], 5 → 2
+    [1,3,5,6], 2 → 1
+    [1,3,5,6], 7 → 4
+    [1,3,5,6], 0 → 0
+"""
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        l = len(nums)
+        mid = l / 2
+        low = 0
+        high = l - 1
+        while 1:
+            if low > high:
+                mid += 1
+                break
+            if target == nums[mid]:
+                break
+            elif target < nums[mid]:
+                high = mid - 1
+                mid = (low + high) / 2
+            else:
+                low = mid + 1
+                mid = (low + high) / 2
+
+        return mid
