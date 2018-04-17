3a36671c2046f543ca6d35ec59435ccbdf4dc81f
cancan_work <1048042379@qq.com>
2018-04-17 09:36:57 +0800

    补充第217题的题目信息

--- primary/array/question_4.py
+++ primary/array/question_4.py
@@ -4,6 +4,11 @@
 # @File     : question_2.py
 # @Function : 存在重复
 
+"""
+Question:
+给定一个整数数组，判断是否存在重复元素。
+如果任何值在数组中出现至少两次，函数应该返回 true。如果每个元素都不相同，则返回 false。
+"""
 
 class Solution:
     def containsDuplicate(self, nums):
