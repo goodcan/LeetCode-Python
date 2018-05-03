a35d639c3432603877c35bc31c08bcd0dab3df90
cancan_work <1048042379@qq.com>
2018-05-03 10:01:08 +0800

    第344题反转字符串

---
+++ questionBank/344.Reverse String/method_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2018/5/3 9:58
+# @Author   : cancan
+# @File     : method_1.py
+# @Function : 反转字符串
+
+"""
+Question:
+请编写一个函数，其功能是将输入的字符串反转过来。
+
+Example:
+输入：s = "hello"
+返回："olleh"
+"""
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
