87797bd6748fece8d49e4042b95d18fabfd494bb
cancan <1048042379@qq.com>
2018-05-05 13:29:10 +0800

    第14题最长公共前缀

---
+++ primary/Strings/question_9.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# @Time     : 2018/5/5 下午1:03
+# @Author   : cancan
+# @File     : question_9.py
+# @Function : 最长公共前缀
+
+"""
+Question:
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+
+Example 1:
+输入: ["flower","flow","flight"]
+输出: "fl"
+
+Example 2:
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+
+Note:
+所有输入只包含小写字母 a-z 。
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        t = min(strs, key=lambda x: len(x)) if strs else ''
+        l = len(t)
+        for i in strs:
+            if t == i:
+                continue
+            for y in range(l):
+                if t[:y+1] != i[:y+1]:
+                    t = '' if y == 0 else t[:y]
+                    break
+        return t
+
+
+if __name__ == "__main__":
+    a = Solution()
+    # s = ["flower","flow","flight"]
+    s = ["ca","a"]
+    print(a.longestCommonPrefix(s))
