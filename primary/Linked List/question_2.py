609da881ee60f2bc59e53f947512e8f2a97b35b4
cancan <1048042379@qq.com>
2018-05-05 15:05:41 +0800

    第19题删除链表的倒数第N个节点

---
+++ primary/Linked List/question_2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# @Time     : 2018/5/5 下午1:53
+# @Author   : cancan
+# @File     : question_2.py
+# @Function : 删除链表的倒数第N个节点
+
+"""
+Question:
+给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+Example:
+给定一个链表: 1->2->3->4->5, 和 n = 2.
+当删除了倒数第二个节点后，链表变为 1->2->3->5.
+
+Note:
+给定的 n 保证是有效的。
+
+Follow up:
+你能尝试使用一趟扫描实现吗？
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        if not head.next:
+            return None
+        nodes = [head]
+        t = head.next
+        while t:
+            nodes.append(t)
+            t = t.next
+        if n == 1:
+            nodes[-2].next = None
+        else:
+            nodes[-n].val = nodes[-n].next.val
+            nodes[-n].next = nodes[-n].next.next
+
+        return head
+
+
+if __name__ == "__main__":
+    a = Solution()
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l1.next = l2
+    a.removeNthFromEnd(l1, 1)
+    print(l1.next)
