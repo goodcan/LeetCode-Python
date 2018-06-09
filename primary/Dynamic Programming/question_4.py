e6a8dac25e32b5ccaaf651ab3c83022a0db52c6a
cancan <1048042379@qq.com>
2018-06-09 18:36:20 +0800

    第198题打家劫舍

---
+++ primary/Dynamic Programming/question_4.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# @Time     : 2018/6/9 下午6:04
+# @Author   : cancan
+# @File     : question_4.py
+# @Function : 打家劫舍
+
+"""
+Question:
+你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，
+能够偷窃到的最高金额。
+
+Example 1:
+输入: [1,2,3,1]
+输出: 4
+解释: 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+
+Example 2:
+输入: [2,7,9,3,1]
+输出: 12
+解释: 偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+"""
+
+
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = len(nums)
+        if l == 0:
+            return 0
+        elif l == 1:
+            return nums[0]
+        elif l == 2:
+            return max(nums)
+
+        g = t = 0
+        for i in nums:
+            temp = g
+            g = max(g, t + i)
+            t = temp
+
+        return g
+
