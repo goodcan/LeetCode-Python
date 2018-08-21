553b4707228af4f94e8a64026a81817369d6ea38
cancan_work <1048042379@qq.com>
2018-08-21 11:19:19 +0800

    学习 数据和字符串 小结 旋转数据

--- Learn/Array and String/Conclusion/question_1.py
+++ Learn/Array and String/Conclusion/question_1.py
@@ -24,4 +24,4 @@ class Solution:
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         l = len(nums)
-        nums[:k], nums[k: l] = nums[l-k: l], nums[: l-k]
+        nums[:k], nums[k: l] = nums[l - k: l], nums[: l - k]
