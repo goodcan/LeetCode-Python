6b10429e4478722e6611929e8a39623d5d754da0
灿灿 <1048042379@qq.com>
2019-03-22 13:33:15 +0800

    第766题托普利茨矩阵

--- QuestionBank/766.Toeplitz Matrix/method_1.py
+++ QuestionBank/766.Toeplitz Matrix/method_1.py
@@ -51,7 +51,7 @@ class Solution:
         n = len(matrix)
 
         for i in range(m - 1):
-            for j in range(0, n - 1):
+            for j in range(n - 1):
                 if matrix[j][i] != matrix[j + 1][i + 1]:
                     return False
 
