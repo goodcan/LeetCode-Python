3c90bb6383036c9123a99dbfc8857a452edae597
cancan <1048042379@qq.com>
2019-06-23 13:56:54 +0800

    第1002题查找常用字符串

---
+++ QuestionBank/1002.Find Common Characters/method_1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2019-06-23 12:53
+# @Author   : cancan
+# @File     : method_1.py
+# @Function : 查找常用字符
+
+"""
+Question:
+给定仅有小写字母组成的字符串数组 A，返回列表中的每个字符串中都显示的全部字符（包括重复字符）
+组成的列表。例如，如果一个字符在每个字符串中出现 3 次，但不是 4 次，则需要在最终答案中包含该字符 3 次。
+你可以按任意顺序返回答案。
+
+示例 1：
+输入：["bella","label","roller"]
+输出：["e","l","l"]
+
+示例 2：
+输入：["cool","lock","cook"]
+输出：["c","o"]
+ 
+提示：
+1 <= A.length <= 100
+1 <= A[i].length <= 100
+A[i][j] 是小写字母
+"""
+
+
+class Solution:
+    def commonChars(self, A):
+        tmp1 = {}
+
+        for v in A.pop():
+            tmp1.setdefault(v, 0)
+            tmp1[v] += 1
+
+        while len(A) != 0:
+            a = A.pop()
+            tmp2 = {}
+            for v in a:
+                tmp2.setdefault(v, 0)
+                tmp2[v] += 1
+
+            tmp3 = {}
+            for k, v in tmp2.items():
+                if k in tmp1:
+                    tmp3[k] = tmp1[k]
+
+                    if v < tmp1[k]:
+                        tmp3[k] = v
+
+            tmp1 = tmp3
+
+        ret = []
+
+        for k, v in tmp1.items():
+            ret.extend([k] * v)
+
+        return ret
--- README.md
+++ README.md
@@ -395,6 +395,7 @@
 985 [查询后的偶数和](./QuestionBank/985.Sum%20of%20Even%20Numbers%20After%20Queries/)<br>
 989 [数组形式的整数加法](./QuestionBank/989.Add%20to%20Array-Form%20of%20Integer/)<br>
 993 [二叉树的堂兄弟节点](./QuestionBank/993.Cousins%20in%20Binary%20Tree/)<br>
+1002 [查找常用字符](./QuestionBank/1002.Find%20Common%20Characters/)<br>
 1012 [十进制整数的反码](./QuestionBank/1012.Complement%20of%20Base%2010%20Integer/)<br>
 1021 [删除最外层的括号](./QuestionBank/1021.Remove%20Outermost%20Parentheses/)<br>
 
