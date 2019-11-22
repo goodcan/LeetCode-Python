05d3d0714e6088ff9e555aad0e2f77be9a57a167
灿灿 <1048042379@qq.com>
2019-11-22 10:51:27 +0800

    [upd] 第141题环形链表

--- QuestionBank/141.Linked List Cycle/method_1.py
+++ QuestionBank/141.Linked List Cycle/method_1.py
@@ -7,11 +7,26 @@
 """
 Question:
 给定一个链表，判断链表中是否有环。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
 
-Follow up:
-你能否不使用额外空间解决此题？
+Example 1：
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+Example 2：
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+Example 3：
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+Follow up：
+你能用 O(1)（即，常量）内存解决此问题吗？
 """
-
 
 # Definition for singly-linked list.
 # class ListNode(object):
