c1ac26baab27bb09e93798c9c84fca2c3f2f7357
cancan <1048042379@qq.com>
2020-07-20 21:29:09 +0800

    [dev] 第1281题整数的各位积和之差

---
+++ QuestionBank/1281.Subtract the Product and Sum of Digits of an Integer/method_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2020/7/20 21:27
+# @Author   : cancan
+# @File     : method_1.py
+# @Function : 整数的各位积和之差
+
+"""
+给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+
+示例 1：
+输入：n = 234
+输出：15
+解释：
+各位数之积 = 2 * 3 * 4 = 24
+各位数之和 = 2 + 3 + 4 = 9
+结果 = 24 - 9 = 15
+
+示例 2：
+输入：n = 4421
+输出：21
+解释：
+各位数之积 = 4 * 4 * 2 * 1 = 32
+各位数之和 = 4 + 4 + 2 + 1 = 11
+结果 = 32 - 11 = 21
+ 
+提示：
+1 <= n <= 10^5
+"""
+
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        a, b = divmod(n, 10)
+        mul = b
+        add = b
+        while a > 0:
+            a, b = divmod(a, 10)
+            mul *= b
+            add += b
+        return mul - add
