2aa8d6259e577c3b363365d12554c52269e0ccd5
灿灿 <1048042379@qq.com>
2020-09-09 18:41:13 +0800

    [dev] 第77题组合

---
+++ QuestionBank/77.Combinations/method_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2020/9/9 18:38
+# @Author   : cancan
+# @File     : method_1.py
+# @Function : 组合
+
+"""
+给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例:
+输入: n = 4, k = 2
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+"""
+
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.ret = []
+        self.nums = list(range(1, n + 1))
+        self.k = k
+        self.dfs(0, [])
+        return self.ret
+
+    def dfs(self, start, l):
+        if len(l) > self.k:
+            return
+
+        if len(l) == self.k:
+            self.ret.append(l)
+            return
+
+        for i, v in enumerate(self.nums[start:]):
+            self.dfs(i + start + 1, l + [v])
