579fc61cedcf3cd9c32b7b2534d04672ed5f9457
cancan <1048042379@qq.com>
2021-02-07 10:34:34 +0800

    [dev] 第665题非递减数列

---
+++ QuestionBank/665.Non-decreasing Array/method_1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2021/2/7 10:03
+# @Author   : cancan
+# @File     : method_1.py
+# @Function : 非递减数列
+
+"""
+给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+
+示例 1:
+输入: nums = [4,2,3]
+输出: true
+解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+
+示例 2:
+输入: nums = [4,2,1]
+输出: false
+解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+
+说明：
+1 <= n <= 10 ^ 4
+- 10 ^ 5 <= nums[i] <= 10 ^ 5
+"""
+
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                count += 1
+                if count > 1:
+                    return False
+                if i == 1 or nums[i] >= nums[i - 2]:
+                    nums[i - 1] = nums[i]
+                else:
+                    nums[i] = nums[i - 1]
+        return True
--- README.md
+++ README.md
@@ -385,6 +385,7 @@
 648 [单词替换](./QuestionBank/648.Replace%20Words/)<br>
 657 [机器人能否返回原点](./QuestionBank/657.Robot%20Return%20to%20Origin/)<br>
 658 [找到 K 个最接近的元素](./QuestionBank/658.Find%20K%20Closest%20Elements/)<br>
+665 [非递减数列](./QuestionBank/665.Non-decreasing%20Array/)<br>
 674 [最长连续递增序列](./QuestionBank/674.Longest%20Continuous%20Increasing%20Subsequence/)<br>
 680 [验证回文字符串 Ⅱ](./QuestionBank/680.Valid%20Palindrome%20II/)<br>
 682 [棒球比赛](./QuestionBank/682.Baseball%20Game/)<br>
